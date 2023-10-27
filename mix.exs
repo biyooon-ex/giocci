e08e37df5f0b9c05fbe417969fa3a56d45c27bd8
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2023-10-27 15:05:42 +0900

    ready to publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -1,10 +1,21 @@
 defmodule Giocci.MixProject do
   use Mix.Project
 
+  @description """
+  Client Library for Giocci (computational resource permeating wide-area distributed platform towards the B5G era)
+  """
+
+  @version "0.1.0"
+  @source_url "https://github.com/b5g-ex/giocci"
+
   def project do
     [
       app: :giocci,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
+      package: package(),
+      name: "Giocci",
+      docs: docs(),
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,6 +34,29 @@ defmodule Giocci.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    %{
+      name: "giocci",
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{"Github" => @source_url}
+    }
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 end
