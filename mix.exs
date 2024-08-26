fd858c91ada686b368890f1a1bd7d522186bb8d1
NISHIUCHI Kazuma <nishiuchi.kazuma@gmail.com>
2024-08-26 23:46:34 +0900

    upd: version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Giocci.MixProject do
   Client Library for Giocci (computational resource permeating wide-area distributed platform towards the B5G era)
   """
 
-  @version "0.2.1"
+  @version "0.2.2"
   @source_url "https://github.com/b5g-ex/giocci"
 
   def project do
