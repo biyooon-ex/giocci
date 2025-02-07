2d01ec9aed3c9bd8b185ff543c60776a27bce3ae
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2025-02-07 19:37:36 +0900

    bump version to v0.3.0-rc2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Giocci.MixProject do
   Client Library for Giocci (computational resource permeating wide-area distributed platform towards the B5G era)
   """
 
-  @version "0.3.0-rc1"
+  @version "0.3.0-rc2"
   @source_url "https://github.com/b5g-ex/giocci"
 
   def project do
